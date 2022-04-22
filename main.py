e866e340b629f9b78ef0cb7bf0598918b693f8e0
chudinova <chudinova-olimp@ya.ru>
2022-04-22 16:26:48 +0500

    М: Посдедний коммит?

--- main.py
+++ main.py
@@ -477,7 +477,7 @@ def about_pg():
 
 
 if __name__ == '__main__':
+    """Подключаемся к БД"""
     db_session.global_init("db/posts.db")
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    # app.run(port=8080, host='127.0.1.1')
+    """Запускаем приложение"""
+    app.run(port=8080, host='127.0.0.1')
